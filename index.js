f0097b27f5661a338db31c639ed1d3c855b4670f
Johnny Domino <jvdomino@gmail.com>
2020-04-03 22:59:40 -0400

    update to support conference rooms

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const io = require('socket.io')()
 
 io.on('connection', socket => {
-  socket.on('join', ({signalId}) => {
-    socket.join(signalId)
-    socket.on('signal', (signal) => {
-      socket.broadcast.to(signalId).emit('signal', signal)
-    })
+
+  socket.on('signal', (remotePeerId, signal) => {
+    io.to(remotePeerId).emit('signal', socket.id, signal)
   })
+
+  socket.on('join', ({channelSecret}) => {
+    socket.join(channelSecret)
+  })
+
+  socket.on('aloha', ({channelSecret}) => {
+    socket.to(channelSecret).emit('invite', socket.id)
+  })
+
 })
 
 io.listen(process.env.PORT || 3000)
