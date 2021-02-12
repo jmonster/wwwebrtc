e0cc05dbe603117bb8b49137a4a1ccb0975de464
Johnny Domino <blah@hey.com>
2021-02-12 17:04:18 -0500

    boy, I tell you h’wat

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
-const io = require('socket.io')()
+const httpServer = require('http').createServer();
+const io = require('socket.io')(httpServer, {
+  cors: {
+    origin: 'http://localhost:8000',
+    methods: ['GET', 'POST']
+  }
+});
 
 // when a client (browser) connects to the server
-io.on('connection', socket => {
+io.on('connection', async (socket) => {
+  const {channel} = socket.handshake.query
 
-  // server receives a 'signal' addressed to 'remotePeerId' and replays it to them
-  // the sender of this message is implied by whomever is connected on 'socket'
-  // i.e. the sender is 'socket.id'
+  // add this client to the channel they requested
+  socket.join(channel)
+
+  // emit all active peer ids in this channel
+  const ids = Array.from(await io.in(channel).allSockets())
+  socket.emit('participants', ids)
+
+  // relay signals between peers
   socket.on('signal', (remotePeerId, signal) => {
     io.to(remotePeerId).emit('signal', socket.id, signal)
   })
 
-  // After a browser connects, they may send a 'join' message.
-  // 'channelSecret' represents a particular/unique multiparty call.
-  socket.on('join', async ({channelSecret}) => {
-    // all sockets in the 'call' namespace and in the 'channelSecret' room
-    const ids = await io.in(channelSecret).allSockets()
-    // TODO omit current socket from collection?
-    socket.emit('peersAvailable', ids)
-    socket.join(channelSecret)
-  })
-  
+  // debugging
+  // socket.onAny((event) => {
+  //   console.debug(`got ${event}`);
+  // });
+
 })
 
 io.listen(process.env.PORT || 3000)
