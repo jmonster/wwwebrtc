618ad5b1c793d019d6c3feffb380e1cd2356a154
Johnny Domino <blah@hey.com>
2021-02-21 14:18:45 -0500

    cors wildcard

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const httpServer = require('http').createServer();
 const io = require('socket.io')(httpServer, {
   cors: {
-    origin: 'http://localhost:8000',
+    origin: '*',
     methods: ['GET', 'POST']
   }
 });
